a36e6c49a870812a838a0a62b00056cfcaae48e6
Eusébio Salomão <eusebiosalomao@outlook.com>
2023-06-29 18:28:09 +0100

    Banco connect

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const connectDB = () => {
     console.log("Aguardando a conexão com o Mongodb Atla...")
 
     mongoose.Promise = global.Promise;
-    mongoose.connect("mongodb+srv://alomao:terebango@cluster0.rmvzzy3.mongodb.net/?retryWrites=true&w=majority").then(() => {
+    mongoose.connect("mongodb+srv://salomao:terebango@cluster0.rmvzzy3.mongodb.net/?retryWrites=true&w=majority").then(() => {
         console.log("BD SS Mongo Atlas conectado em online!")
     }).catch((erro) => {
         console.log("Erro de Conexão com Mongo Atlas: " + erro)
